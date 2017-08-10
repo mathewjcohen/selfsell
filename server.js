8df36904f0f1779bbd350ed27fb805212a4c9d48
mathew <mathewjcohen@gmail.com>
2017-08-10 08:57:15 -0700

    removed angular from protfolio as it ended up being unnecessary for this version

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+// this snippet should be in: >ProjectDir/server.js
 const express = require('express');
 const path = require('path');
-const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json()); // for sending json
-app.use(express.static(path.join(__dirname, './public/dist')));
+const app = express();
+
+app.use(express.static(__dirname + "/client/static"));
+
+app.set("views", path.join(__dirname, "./client/views"));
+app.set('view engine', 'ejs');
+
+var routes_setter = require('./server/config/routes.js');
+routes_setter(app);
 
 app.listen(8000);
