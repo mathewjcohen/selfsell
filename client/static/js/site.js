e2b004b2df3af51ef7f63ae1251ca1b77e6c4200
mathew <mathewjcohen@gmail.com>
2017-10-05 12:31:22 -0700

    removed some externally linked image sources and made them local; updated a couple html elements for compliance reasons.

--- client/static/js/site.js
+++ client/static/js/site.js
@@ -1,32 +1,34 @@
-$(document).ready(function () {
-    // navigation click move-to functionality
-    $('a[href*="#"]')
-        .click(function (event) {
-            // On-page links
-            if (
-                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-                &&
-                location.hostname == this.hostname
-            ) {
-                // Figure out element to scroll to
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                // Does a scroll target exist?
-                if (target.length) {
-                    // Only prevent default if animation is actually gonna happen
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 500);
-                }
-            }
-        });
+$(document).ready(function() {
+  // navigation click move-to functionality
+  $('a[href*="#"]').click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          500
+        );
+      }
+    }
+  });
 
-    $(".menu_toggle").on('click', function () {
-        $(this).toggleClass("on");
-        $("nav ul").toggleClass('hidden');
-    });
-    $("nav ul li").click( function(){
-        $(this).toggleClass("animated swing");
-    })
-})
+  $(".menu_toggle").on("click", function() {
+    $(this).toggleClass("on");
+    $("nav ul").toggleClass("hidden");
+  });
+  $("nav ul li").click(function() {
+    $(this).toggleClass("animated swing");
+  });
+});
