0bc745bcbe6db5646e403fd8acb3bc0a1b6548f4
mathew <mathewjcohen@gmail.com>
2017-10-10 10:11:47 -0700

    complete redesign; still need a new profile image

--- client/static/js/site.js
+++ client/static/js/site.js
@@ -1,24 +1,27 @@
 $(document).ready(function() {
-  // navigation click move-to functionality
   $('a[href*="#"]').click(function(event) {
-    // On-page links
     if (
       location.pathname.replace(/^\//, "") ==
         this.pathname.replace(/^\//, "") &&
       location.hostname == this.hostname
     ) {
-      // Figure out element to scroll to
       var target = $(this.hash);
       target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-      // Does a scroll target exist?
       if (target.length) {
-        // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $("html, body").animate({ scrollTop: target.offset().top }, 500);
       }
     }
   });
 
+  $(window).scroll(function(){
+    var wScroll = $(window).scrollTop();
+    if(wScroll > 150){
+      $("nav").addClass("nav_bg");
+    }else{
+      $("nav").removeClass("nav_bg");
+    }
+  })
   $("nav ul li").click(function() {
     $(this).toggleClass("animated swing");
   });
