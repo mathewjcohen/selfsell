8caeb49bd138f1d00a62ccd968ab3feb864edb56
Mathew Cohen <mathewjcohen@gmail.com>
2020-05-28 15:03:25 -0700

    add baby shower page

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,3 +1,4 @@
-module.exports = (app) => {
-    app.get('/', (req, res) => res.render('index'))
-}
+module.exports = app => {
+  app.get("/", (req, res) => res.render("index"));
+  app.get("/mc2-plus-1", (req, res) => res.render("baby"));
+};
