7af5ce2df49a5d86a2cd27ca1e88c7ff1adbfea2
Mathew Cohen <mathewjcohen@gmail.com>
2020-06-18 17:00:04 -0700

    remove feedrate; still not updating vals on keyup

--- client/static/js/tools.js
+++ client/static/js/tools.js
@@ -8,18 +8,18 @@
 
 // grab inputs and buttons
 let jsGcodeFacingDir = document.getElementById("jsGcodeFacingDir").value;
+const jsGcodeFacingBit = document.getElementById("jsGcodeFacingBit").value;
+const jsGcodeFacingWidth = document.getElementById("jsGcodeFacingWidth").value;
+const jsGcodeFacingLength = document.getElementById("jsGcodeFacingLength")
+  .value;
+const jsGcodeFacingOutput = document.getElementById("jsGcodeFacingOutput");
+
 document
   .getElementById("jsGcodeFacingDir")
   .addEventListener("change", function() {
     jsGcodeFacingDir = document.getElementById("jsGcodeFacingDir").value;
   });
-const jsGcodeFacingBit = document.getElementById("jsGcodeFacingBit").value;
-const jsGcodeFacingFeedrate = document.getElementById("jsGcodeFacingFeedrate")
-  .value;
-const jsGcodeFacingWidth = document.getElementById("jsGcodeFacingWidth").value;
-const jsGcodeFacingLength = document.getElementById("jsGcodeFacingLength")
-  .value;
-const jsGcodeFacingOutput = document.getElementById("jsGcodeFacingOutput");
+
 document
   .getElementById("jsGcodeFacingBtn")
   .addEventListener("click", function() {
@@ -61,22 +61,21 @@ function generateGcode() {
 // generate gcode when xtravel is the facing cut
 function xTravelCut() {
   let p = 0;
-  let fr = jsGcodeFacingFeedrate;
   let output = [];
   const halfXwidth = jsGcodeFacingWidth / 2;
   while (p < jsGcodeFacingLength) {
-    output.push(`G0 X${halfXwidth} F${fr}`); // move to X pos
+    output.push(`G0 X${halfXwidth}`); // move to X pos
     p += parseInt(jsGcodeFacingBit); // set new Y pos
     if (p > jsGcodeFacingWidth) {
       return output;
     }
-    output.push(`G0 Y${p} F${fr}`); // move to new Y pos
-    output.push(`G0 X-${halfXwidth} F${fr}`); // move to X pos
+    output.push(`G0 Y${p}`); // move to new Y pos
+    output.push(`G0 X-${halfXwidth}`); // move to X pos
     p += parseInt(jsGcodeFacingBit); // set new Y pos
     if (p > jsGcodeFacingWidth) {
       return output;
     }
-    output.push(`G0 Y${p} F${fr}`); // move to new Y pos
+    output.push(`G0 Y${p}`); // move to new Y pos
   }
   return output;
 }
@@ -84,21 +83,20 @@ function xTravelCut() {
 // generate gcode when ytravel is the facing cut
 function yTravelCut() {
   let p = (jsGcodeFacingWidth / 2) * -1;
-  let fr = jsGcodeFacingFeedrate;
   let output = [];
   while (p <= jsGcodeFacingWidth) {
-    output.push(`G0 Y${jsGcodeFacingLength} F${fr}`); // move Y pos end
+    output.push(`G0 Y${jsGcodeFacingLength}`); // move Y pos end
     p += parseInt(jsGcodeFacingBit); // set new X pos
     if (p > jsGcodeFacingWidth) {
       return output;
     }
-    output.push(`G0 X${p} F${fr}`); // move to new X pos
-    output.push(`G0 Y0 F${fr}`); // move to Y start
+    output.push(`G0 X${p}`); // move to new X pos
+    output.push(`G0 Y0`); // move to Y start
     p += parseInt(jsGcodeFacingBit); // set new X pos
     if (p > jsGcodeFacingWidth) {
       return output;
     }
-    output.push(`G0 X${p} F${fr}`); // move to new X pos
+    output.push(`G0 X${p}`); // move to new X pos
   }
   return output;
 }
